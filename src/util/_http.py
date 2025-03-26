37df691fa0c4950f895131bd9fdf664cbdb0e152
von-steinkirch <b@bt3gl.xyz>
2025-03-26 13:37:28 +0100

    clean up _init

--- src/util/_http.py
+++ src/util/_http.py
@@ -11,14 +11,15 @@ class DefaultAsyncHttpxClient(httpx.AsyncClient):
     def __init__(
         self,
         *,
-        timeout: float | httpx.Timeout | None = 60.0,
+        timeout: float | httpx.Timeout | None = 120.0,
         verify: bool | str = True,
         http2: bool = True,
         **kwargs: Any,
     ) -> None:
         super().__init__(
-            timeout=timeout,
+            timeout=httpx.Timeout(timeout, connect=30.0, read=90.0),
             verify=verify,
             http2=http2,
+            limits=httpx.Limits(max_keepalive_connections=5, max_connections=10),
             **kwargs,
         )
